aa9ad344d882a52e75089c93d52de8cc79cb1e58
Ant0yne <antoine.soliv@gmail.com>
2024-05-13 15:07:42 +0200

    tuple

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,12 @@ import Button from "./Button";
 function App() {
 	return (
 		<div>
-			<Button backGroundColor="red" fontSize={50} pillShape={true} />
+			<Button
+				backGroundColor="red"
+				fontSize={50}
+				pillShape={true}
+				padding={[5, 10, 20, 15]}
+			/>
 		</div>
 	);
 }
--- src/Button.tsx
+++ src/Button.tsx
@@ -2,16 +2,21 @@ const convertCurrency = (amount: number, currency: string): string => {
 	return "test";
 };
 
+type Color = "red" | "blue" | "green";
+
 type ButtonProps = {
-	backGroundColor?: "red" | "blue" | "green";
+	backGroundColor?: Color;
+	textColor?: Color;
 	fontSize?: number;
 	pillShape?: boolean;
+	padding: [number, number, number, number];
 };
 
 const Button = ({
 	backGroundColor = "blue",
 	fontSize = 20,
 	pillShape = false,
+	padding = [0, 0, 0, 0],
 }: ButtonProps) => {
 	let url = "https://duckduckgo.com/";
 
@@ -24,6 +29,7 @@ const Button = ({
 					backgroundColor: backGroundColor,
 					fontSize: fontSize,
 					borderRadius: pillShape ? "10" : "0",
+					padding: `${padding[0]}px ${padding[1]}px ${padding[2]}px ${padding[3]}px`,
 				}}>
 				I am a button
 			</button>
