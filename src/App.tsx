f35d918c392cadcfb04d907b3194b46ace599acc
Ant0yne <antoine.soliv@gmail.com>
2024-05-13 15:26:15 +0200

    children

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,27 @@ import "./App.css";
 import Button from "./Button";
 
 function App() {
+	const onClick = () => {
+		return 5;
+	};
+
 	return (
 		<div>
 			<Button
-				backGroundColor="red"
-				fontSize={50}
-				pillShape={true}
-				padding={[5, 10, 20, 15]}
-			/>
+				style={{
+					backgroundColor: "red",
+					fontSize: 50,
+					padding: "5px, 10px",
+				}}
+				borderRadius={{
+					topLeft: 5,
+					topRight: 5,
+					bottomLeft: 10,
+					bottomRight: 10,
+				}}
+				onClick={onClick}>
+				I am a button
+			</Button>
 		</div>
 	);
 }
--- src/Button.tsx
+++ src/Button.tsx
@@ -2,36 +2,32 @@ const convertCurrency = (amount: number, currency: string): string => {
 	return "test";
 };
 
-type Color = "red" | "blue" | "green";
+// type Color = "red" | "blue" | "green";
+
+// type ButtonProps = {
+// 	backGroundColor?: Color;
+// 	textColor?: Color;
+// 	fontSize?: number;
+// 	pillShape?: boolean;
+// 	padding: [number, number, number, number];
+// };
 
 type ButtonProps = {
-	backGroundColor?: Color;
-	textColor?: Color;
-	fontSize?: number;
-	pillShape?: boolean;
-	padding: [number, number, number, number];
+	style: React.CSSProperties;
+	borderRadius: Record<string, number>;
+	onClick: () => number;
+	children: React.ReactNode;
 };
 
-const Button = ({
-	backGroundColor = "blue",
-	fontSize = 20,
-	pillShape = false,
-	padding = [0, 0, 0, 0],
-}: ButtonProps) => {
+const Button = ({ style, borderRadius, onClick, children }: ButtonProps) => {
 	let url = "https://duckduckgo.com/";
 
 	convertCurrency(100, "EUR");
 
 	return (
 		<div>
-			<button
-				style={{
-					backgroundColor: backGroundColor,
-					fontSize: fontSize,
-					borderRadius: pillShape ? "10" : "0",
-					padding: `${padding[0]}px ${padding[1]}px ${padding[2]}px ${padding[3]}px`,
-				}}>
-				I am a button
+			<button onClick={onClick} style={style}>
+				{children}
 			</button>
 		</div>
 	);
