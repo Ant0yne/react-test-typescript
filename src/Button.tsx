df3cbe693a7ceef5b3232578e689ff9e154b1e62
Ant0yne <antoine.soliv@gmail.com>
2024-05-13 14:34:53 +0200

    first commit

---
+++ src/Button.tsx
@@ -0,0 +1,9 @@
+const convertCurrency = (amount: number, currency: string) => {};
+
+const Button = () => {
+	let url = "https://duckduckgo.com/";
+
+	return <div>Button</div>;
+};
+
+export default Button;
