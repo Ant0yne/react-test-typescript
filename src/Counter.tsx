2da81eb2b34bddf7940bdf19fdd0334418c49355
Ant0yne <antoine.soliv@gmail.com>
2024-05-14 16:07:24 +0200

    last

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { type Color } from "./lib/types";
 
 type CounterProps<T> = {
@@ -6,6 +7,13 @@ type CounterProps<T> = {
 };
 
 const Counter = <T,>({ countValue, countHistory }: CounterProps<T>) => {
+	const value = countValue;
+
+	useEffect(() => {
+		fetch("https://jsonplaceholder.typicode.com/todos/1")
+			.then((response) => response.json())
+			.then((json: unknown) => console.log(json));
+	}, []);
 	return <div></div>;
 };
 
